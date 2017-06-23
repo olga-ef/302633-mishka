44bda0790ed7b03dabcb00a73de66e9cbb228966
Olga Efimova <olya-ef123@mail.ru>
2017-06-23 17:09:50 +0300

    поправила отступы в каталоге и в галпфайле

--- gulpfile.js
+++ gulpfile.js
@@ -90,12 +90,12 @@ gulp.task("clean", function() {
 });
 
 gulp.task("build", function(fn) {
-  run(
-   "clean",
-   "copy",
-   "style",
-   "images",
-   "sprite",
-   fn
+    run(
+     "clean",
+     "copy",
+     "style",
+     "images",
+     "sprite",
+     fn
   );
 });
