02bc3f3f31bd27e9bef4dacf5baba493f07aedfc
Olga Efimova <olya-ef123@mail.ru>
2017-06-23 16:59:41 +0300

    поправки

--- gulpfile.js
+++ gulpfile.js
@@ -86,16 +86,16 @@ gulp.task("copy", function() {
 });
 
 gulp.task("clean", function() {
- return del("build");
+  return del("build");
 });
 
 gulp.task("build", function(fn) {
- run(
- "clean",
- "copy",
- "style",
- "images",
- "sprite",
- fn
+  run(
+   "clean",
+   "copy",
+   "style",
+   "images",
+   "sprite",
+   fn
   );
 });
