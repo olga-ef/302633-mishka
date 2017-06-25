1b423adbe2e80af2532c765c3da0e92e547c2ff8
Olga Efimova <olya-ef123@mail.ru>
2017-06-25 22:57:20 +0300

    заменила picturefill, дописала js для меню

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,9 +1,14 @@
 var toggle = document.querySelector(".main-nav__toggle");
-var menu = document.querySelector(".main-nav--opened");
-var user = document.querySelector(".user-nav--opened");
+var menu = document.querySelector(".main-nav");
+var user = document.querySelector(".user-nav");
+var pageHeader = document.querySelector(".page-header");
+
+pageHeader.classList.remove("page-header--nojs");
+menu.classList.remove("main-nav--nojs");
+user.classList.remove("user-nav--nojs");
 
 toggle.addEventListener("click", function(event) {
   event.preventDefault();
-  menu.classList.toggle("main-nav--closed");
-  user.classList.toggle("user-nav--closed");
+  menu.classList.toggle("main-nav--opened");
+  user.classList.toggle("user-nav--opened");
 });
